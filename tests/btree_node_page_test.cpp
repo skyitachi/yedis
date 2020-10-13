57c53460b08d9076a034b3008b79733f9a178494
yaoshiping <yaoshiping@bytedance.com>
2020-10-13 10:10:02 +0800

    make unit test works

---
+++ tests/btree_node_page_test.cpp
@@ -0,0 +1,16 @@
+//
+// Created by admin on 2020/10/13.
+//
+
+#include <btree_node_page.h>
+#include <gtest/gtest.h>
+
+namespace yedis {
+ class BTreeNodePageTest: public testing::Test {
+
+ };
+}
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
