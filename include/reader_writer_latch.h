b97ac0c12c9da1460d9f0c0d54e736c46da8720d
姚士平 <yaoshiping@bytedance.com>
2020-12-02 08:22:10 +0800

    sync code

--- include/reader_writer_latch.h
+++ include/reader_writer_latch.h
@@ -14,10 +14,15 @@ class ReaderWriterLatch {
     void Latch() {
       std::unique_lock<std::mutex> lk(mu_);
       SPDLOG_INFO("acquire lock and waiting: readers {}", readers_);
+      writers_++;
       cond_.wait(lk, [&]{return readers_ == 0;});
       lk.release();
     }
     void UnLatch() {
+      writers_--;
+      if (writers_ > 0) {
+        cond_.notify_one();
+      }
       mu_.unlock();
     }
     void RLatch() {
@@ -35,6 +40,7 @@ class ReaderWriterLatch {
     std::mutex mu_;
     std::condition_variable cond_;
     int readers_ = 0;
+    int writers_ = 0;
 };
 }
 #endif
