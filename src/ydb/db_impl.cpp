a5176fe0ce6520cb0214d19e32d95588a5c4cd99
Shiping Yao <shiping.yao@shopee.com>
2023-03-12 10:44:13 +0800

    sync code

---
+++ src/ydb/db_impl.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Shiping Yao on 2023/3/12.
+//
+#include "db_impl.h"
+
+namespace yedis {
+
+}
---
+++ src/ydb/db_impl.h
@@ -0,0 +1,17 @@
+//
+// Created by Shiping Yao on 2023/3/12.
+//
+
+#ifndef YEDIS_DB_IMPL_H
+#define YEDIS_DB_IMPL_H
+
+#include <db.h>
+
+namespace yedis {
+  class DBImpl: public DB {
+
+  };
+
+}
+
+#endif //YEDIS_DB_IMPL_H
