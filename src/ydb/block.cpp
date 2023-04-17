bdfd66e3cc5ed4c7f3f2e8443d6d6e098e87b666
Shiping Yao <shiping.yao@shopee.com>
2023-04-17 11:13:02 +0800

    sync code

--- src/ydb/block.cpp
+++ src/ydb/block.cpp
@@ -5,8 +5,32 @@
 #include "block.h"
 #include "table_format.h"
 #include "util.hpp"
+#include "comparator.h"
 
 namespace yedis {
+
+static inline const char* DecodeEntry(const char* p, const char* limit,
+                                      uint32_t* shared, uint32_t* non_shared,
+                                      uint32_t* value_length) {
+  if (limit - p < 3) return nullptr;
+  *shared = reinterpret_cast<const uint8_t*>(p)[0];
+  *non_shared = reinterpret_cast<const uint8_t*>(p)[1];
+  *value_length = reinterpret_cast<const uint8_t*>(p)[2];
+  if ((*shared | *non_shared | *value_length) < 128) {
+    // fast path
+    p += 3;
+  } else {
+    if ((p = GetVarint32Ptr(p, limit, shared)) == nullptr) return nullptr;
+    if ((p = GetVarint32Ptr(p, limit, non_shared)) == nullptr) return nullptr;
+    if ((p = GetVarint32Ptr(p, limit, value_length)) == nullptr) return nullptr;
+  }
+
+  // if no enough left space
+  if (static_cast<uint32_t>(limit - p) < (*non_shared + *value_length)) {
+    return nullptr;
+  }
+  return p;
+}
 
 Block::Block(const yedis::BlockContents &contents) {
   size_ = contents.data.size();
@@ -26,6 +50,76 @@ uint32_t Block::NumRestarts() const {
 }
 
 class Block::Iter : public Iterator {
-};
+private:
+  const Comparator* const comparator_;
+  const char* const data_;       // underlying block contents
+  uint32_t const restarts_;      // Offset of restart array (list of fixed32)
+  uint32_t const num_restarts_;  // Number of uint32_t entries in restart array
 
+  // current_ is offset in data_ of current entry.  >= restarts_ if !Valid
+  uint32_t current_;
+  uint32_t restart_index_;  // Index of restart block in which current_ falls
+  std::string key_;
+  Slice value_;
+  Status status_;
+
+  inline int Compare(const Slice& a, const Slice& b) const {
+    return comparator_->Compare(a, b);
+  }
+
+  inline uint32_t NextEntryOffset() const {
+    return (value_.data() + value_.size()) - data_;
+  }
+
+  uint32_t GetRestartPoint(uint32_t idx) {
+    assert(idx < num_restarts_);
+    return DecodeFixed<uint32_t>((void *) (data_ + restarts_ + idx * sizeof(uint32_t)));
+  }
+
+  bool ParseNextKey() {
+    current_ = NextEntryOffset();
+    const char* p = data_ + current_;
+    const char* limit = data_ + restarts_;
+    if (p >= limit) {
+      current_ = restarts_;
+      restart_index_ = num_restarts_;
+      return false;
+    }
+    uint32_t non_shared, shared, value_size;
+
+  }
+public:
+  Iter(const Comparator* comparator, const char* data, uint32_t restarts,
+       uint32_t num_restarts)
+      : comparator_(comparator),
+        data_(data),
+        restarts_(restarts),
+        num_restarts_(num_restarts),
+        current_(restarts_),
+        restart_index_(num_restarts_) {
+    assert(num_restarts_ > 0);
+  }
+
+  bool Valid() const override { return current_ < restarts_; }
+  Status status() const override { return status_; }
+  Slice key() const override {
+    assert(Valid());
+    return key_;
+  }
+  Slice value() const override {
+    assert(Valid());
+    return value_;
+  }
+  void Next() override {
+
+  }
+
+  void Prev() override {
+
+  }
+
+  void Seek(const Slice& target) override {
+
+  }
+};
 }
