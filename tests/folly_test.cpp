d46abc0ba4e9aeebaa01a547f4a8397ecebd38c2
skyitachi <skbysp@gmail.com>
2023-04-20 08:12:06 +0800

    sync code

--- tests/folly_test.cpp
+++ tests/folly_test.cpp
@@ -5,9 +5,31 @@
 #include <gtest/gtest.h>
 #include <thread>
 
-#include <folly/concurrency/MPMCQueue.h>
+#include <folly/MPMCQueue.h>
 
 TEST(FollyConcurrentQueue, Basic) {
+  folly::MPMCQueue<int> queue(10);
+
+  std::thread producer_th([&]{
+    for (int i = 0; i < 100; i++) {
+      while(!queue.write(i)) {
+
+      }
+    }
+  });
+
+  std::thread consumer_th([&] {
+    for (int i = 0; i < 100; i++) {
+      int value;
+      while(!queue.read(value)) {
+
+      }
+      std::cout << "consumed: " << value << std::endl;
+    }
+  });
+
+  producer_th.join();
+  consumer_th.join();
 
 }
 
